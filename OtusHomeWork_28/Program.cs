ce1fe02c6507cf0540420f68a0d9796611489f4d
seriktest <96250381+seriktest@users.noreply.github.com>
2022-11-20 12:22:39 +0300

    invoke methods

--- OtusHomeWork_28/Program.cs
+++ OtusHomeWork_28/Program.cs
@@ -1,7 +1,21 @@
-﻿
+﻿var pathDir = @"C:\Otus\TestDir";
+var pathFile = Path.Combine(pathDir, "TestFile");
+
+var countDir = 2;
+var countFile = 10;
+
+for (int i = 1; i <= countDir; i++)
+{
+    CreateDir(pathDir, i.ToString());
+}
+
+for (int i = 1; i <= countFile; i++)
+{
+    CreateFile(pathFile, i.ToString(), ".txt");
+}
 
 
-
+Console.ReadKey();
 
 
 void CreateDir(string path, string name)
@@ -19,9 +33,9 @@ void CreateDir(string path, string name)
     }
 }
 
-void CreateFile(string path, string name)
+void CreateFile(string path, string name, string ext)
 {
-    var file = Path.Combine(path, name);
+    var file = Path.Combine(path, name + ext);
     
     try
     {
