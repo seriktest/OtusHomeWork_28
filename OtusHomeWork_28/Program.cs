3c949a204d20d3f26d0068ce1b77927f523387b3
seriktest <96250381+seriktest@users.noreply.github.com>
2022-11-20 10:54:51 +0300

    initial

---
+++ OtusHomeWork_28/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+
+
+
+void CreateDir(string path, string name, int count)
+{
+    var dir = new DirectoryInfo(Path.Combine(path, name));
+    for (int i = 0; i < count; i++)
+    {
+        dir.Create();
+    }
+}
